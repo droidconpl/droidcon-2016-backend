92f5669fb37ce5e45e09b1ad3804855dc4688b15
Przemek Jakubczyk <pjakubczyk@gmail.com>
2016-08-06 16:36:20 +0200

    Seralize questions to json

--- backend/schedule/views.py
+++ backend/schedule/views.py
@@ -1,6 +1,38 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .serializers import QuestionSerializer
+from .models import Question
 
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+def question_list(request):
+    questions = Question.objects.all()
+    serializer = QuestionSerializer(questions, many=True)
+    return JSONResponse(serializer.data)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
 
